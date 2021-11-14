527a0266d295a5184b45eca66f9394449afe2ccd
Webdesk-me <atelford@webdesk.me>
2021-11-14 16:12:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
     "fullscreen": true,
     "title": "main window",
     webPreferences: {
