aee6b385a51a6f296786c6fa4aa5a6cab6223c42
Webdesk-me <atelford@webdesk.me>
2021-11-15 14:14:41 -0500

    update

--- Terminal/index.js
+++ Terminal/index.js
@@ -1,4 +1,8 @@
-// In body tag
 $('body').terminal({
-	
+    hello: function(what) {
+        this.echo('Hello, ' + what + '. Wellcome to this terminal.');
+    }
+  },
+    {
+        greetings: greetings.innerHTML + '\n\nShip: SANS504 \nChief Security Officer \nhelp to start. \ntype exit to exit\n\n'
 });
