c130ae60a821382d3dd72b21a5bd57668119b052
Webdesk-me <atelford@webdesk.me>
2021-11-15 15:33:56 -0500

    Update index.js

--- Terminal/index.js
+++ Terminal/index.js
@@ -1,8 +1,13 @@
 var fs = require('fs-extra');
 const homedir = require('os').homedir();
+var d = '/';
+var home = homedir + 'user/admin' + d;
 $('body').terminal({
     help: function() {
         this.echo('ls: list\ncd "dir": change dir\ncat "file": read file\nopen "file": opens file\nrun "app": reun an app\ngit clone: add app or theme\ngit pull: update app or theme');
+    },
+    ls: function(home, d) {
+        this.echo('testing');
     }
   },
     {
