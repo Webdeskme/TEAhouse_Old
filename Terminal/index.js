6cfed2bb484d01782199f781e9d6049be7e67a8b
Webdesk-me <atelford@webdesk.me>
2021-11-15 11:28:11 -0500

    Update index.js

--- Terminal/index.js
+++ Terminal/index.js
@@ -1,11 +1,4 @@
 // In body tag
 $('body').terminal({
-	iam: function (name) {
-		this.echo('Hello, ' + name +
-			'. Welcome to GeeksForGeeks');
-	}
-}, {
-	greetings: 'GeeksForGeeks - A place to'
-			+ ' learn DS, Algo and Computer'
-			+ ' Science for free'
+	
 });
