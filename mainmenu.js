12350b29b800e715302ef25ccd0089eecf0b397c
Webdesk-me <atelford@webdesk.me>
2021-11-14 19:32:12 -0500

    Update mainmenu.js

--- mainmenu.js
+++ mainmenu.js
@@ -88,7 +88,7 @@ const template = [
     submenu: [
       {
         label: 'Learn More',
-        click () { require('electron').shell.openExternal('https://github.com/Webdeskme/wdOS/wiki') }
+        click () { require('electron').shell.openExternal('https://github.com/Webdeskme/scriptOS/wiki') }
       }
     ]
   }
