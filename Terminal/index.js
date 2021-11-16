d9c3570bad9243889d71a7427fb0de95c7f83bd1
Webdesk-me <atelford@webdesk.me>
2021-11-15 21:18:17 -0500

    update

--- Terminal/index.js
+++ Terminal/index.js
@@ -7,7 +7,13 @@ $('body').terminal({
         this.echo('ls: list\ncd "dir": change dir\ncat "file": read file\nopen "file": opens file\nrun "app": reun an app\ngit clone: add app or theme\ngit pull: update app or theme');
     },
     ls: function() {
-        this.echo('testing' + d);
+      fs.readdir(home + 'd, (err, list) => {
+        list = list.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
+        list.forEach(myApps);
+      function myApps(item) {
+        this.echo(item + '\n');
+      }
+      });
     }
   },
     {
