5894614fdae58a74cb730b64157f6faaa2b209e2
Webdesk-me <atelford@webdesk.me>
2021-11-17 15:12:08 -0500

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -76,7 +76,7 @@ fs.readdir('/usr/share/applications/', (err, list) => {
               line2 = line2.slice(5);
               var sid = line2.replace(/\s/g, '');
               //console.log('line2');
-              if(line2 != 'UXTerm' && line2 != 'Htop' && line2 != 'Tint2' && line2 != 'Vim' && line2 != 'Tint2 Settings' && line2 != 'Python (v2.7)' && line2 != 'Python (v3.7)' && line2 != 'New Private Browsing Window' && line2 != 'ClipIt' && line2 != 'Access Prompt' && line2 != 'View file' && line2 != 'Network' && line2 != 'Advanced Network Configuration' && line2 != 'Notification Daemon' && line2 != 'Handler for snap:// URls'){
+              if(line2 != 'UXTerm' && line2 != 'Htop' && line2 != 'Tint2' && line2 != 'Vim' && line2 != 'Tint2 Settings' && line2 != 'Python (v2.7)' && line2 != 'Python (v3.7)' && line2 != 'New Private Browsing Window' && line2 != 'ClipIt' && line2 != 'Access Prompt' && line2 != 'View file' && line2 != 'Network' && line2 != 'Advanced Network Configuration' && line2 != 'Notification Daemon' && line2 != 'Handler for snap:// URIs'){
               $("#apps").append('<li><a class="dropdown-item" href="#" id="' + sid + '" title="' + line + '">' + line2 + '</a></li>');
               $("#" + sid).click(function(){
                 line = line.split(" ")[0];
