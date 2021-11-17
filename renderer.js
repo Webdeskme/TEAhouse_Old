4fab8af5e8e098680310189f563499654be10fbc
Webdesk-me <atelford@webdesk.me>
2021-11-17 06:39:02 -0500

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -78,7 +78,8 @@ fs.readdir('/usr/share/applications/', (err, list) => {
               if(line2 != 'UXTerm' && line2 != 'Htop' && line2 != 'Tint2' && line2 != 'Vim' && line2 != 'Tint2 Settings' && line2 != 'Python (v2.7)' && line2 != 'Python (v3.7)'){
               $("#apps").append('<li><a class="dropdown-item" href="#" id="' + sid + '" title="' + line + '">' + line2 + '</a></li>');
               $("#" + sid).click(function(){
-                exec(line.split(" "[0]));
+                line = line.split(" "[0]);
+                exec(line);
               });
             }
             }
