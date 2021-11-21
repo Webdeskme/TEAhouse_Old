ac42f32ab8d5a5cb96fcc147d11e6dfca5354da6
Webdesk-me <atelford@webdesk.me>
2021-11-21 00:21:57 -0500

    Update index.js

--- Text/index.js
+++ Text/index.js
@@ -1,7 +1,7 @@
-var $ = require( "../jquery" );
+var $ = require( "jquery" );
 window.$ = $;
-require('../bootstrap');
-const homedir = require('../os').homedir();
+require('bootstrap');
+const homedir = require('os').homedir();
 var root = '/Documents/';
 $("#save").click(function(){
   var con = $("#con").text();
