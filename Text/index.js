2d2532c760f41641977236f5aa89d35da14ffaac
Webdesk-me <atelford@webdesk.me>
2021-11-20 23:57:15 -0500

    text

--- Text/index.js
+++ Text/index.js
@@ -1,3 +1,10 @@
 var $ = require( "jquery" );
 window.$ = $;
 require('bootstrap');
+const homedir = require('os').homedir();
+var root = '/Documents/';
+$("#save").click(function(){
+  var con = document.getElementById(con);
+  var title = document.getElementById(title);
+  fs.writeFileSync( homedir + root + title, con);
+});
