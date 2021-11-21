a7ef17458cc4d36a1bf0adbc496b96bfb60d4c27
Webdesk-me <atelford@webdesk.me>
2021-11-21 00:10:18 -0500

    Update index.js

--- Text/index.js
+++ Text/index.js
@@ -6,5 +6,6 @@ var root = '/Documents/';
 $("#save").click(function(){
   var con = $("#con").text();
   var title = $("#title").text();
+  console.log(homedir + root + title);
   fs.writeFileSync( homedir + root + title, con);
 });
