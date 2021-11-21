1551339938262277d448f019cdbdba75dbe00887
Webdesk-me <atelford@webdesk.me>
2021-11-20 20:07:05 -0500

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -4,7 +4,7 @@ require('bootstrap');
 const { exec } = require('child_process');
 var fs = require('fs-extra');
 const homedir = require('os').homedir();
-const { BrowserWindow } = require('@electron/remote')
+const { BrowserWindow } = require('@electron/remote/main').enable(webContents);
 $("#shut").click(function(){
   exec('shutdown -h now');
 });
