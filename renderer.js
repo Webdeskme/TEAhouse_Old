54ceafd8ae58353141fcabc6710686756fe1644f
Webdesk-me <atelford@webdesk.me>
2021-11-20 20:34:32 -0500

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -32,7 +32,8 @@ function myApps(item) {
   $("#apps").append('<li><a class="dropdown-item" href="#" id="' + item + '">' + item + '</a></li>');
   $("#" + item).click(function(){
     //window.open('homedir + '/Apps/' + item + '/index.html', '_blank', 'top=500,left=200,frame=true, enableRemoteModule: true, nodeIntegration: true, contextIsolation: false');
-    createBrowserappWindow(homedir + '/Apps/' + item);
+console.log(homedir + '/Apps/Tea/' + item);
+    createBrowserappWindow(homedir + '/Apps/Tea/' + item);
     //console.log('test');
   });
   function createBrowserappWindow(url1) {
