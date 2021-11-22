0dcf87e381e8399c76978822a77e0ada70c10267
Webdesk-me <atelford@webdesk.me>
2021-11-22 11:22:38 -0500

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -5,7 +5,7 @@ const { exec } = require('child_process');
 var fs = require('fs-extra');
 const homedir = require('os').homedir();
 const { BrowserWindow } = require('@electron/remote');
-var internalIpV4 = require('internal-ip').internalIpV4();
+var { internalIpV4 } = require('internal-ip');
 //import {internalIpV6, internalIpV4} from 'internal-ip';
 $('#ip').text(internalIpV4());
 $("#shut").click(function(){
